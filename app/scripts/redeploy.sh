bc421290c9ac48f33aa8d8dbee17a7b3bd80ac32
dmosc <osdavrm@gmail.com>
2023-02-25 23:50:05 -0600

    Add systemctl commands to reset the service.

--- app/scripts/redeploy.sh
+++ app/scripts/redeploy.sh
@@ -4,3 +4,5 @@ cd ~/repos/project-placeholder;
 git stash && git switch main && git fetch --all && git reset --hard origin/main;
 source venv/bin/activate;
 pip install -q -r requirements.txt;
+systemctl daemon-reload;
+systemctl restart project-placeholder.service;
